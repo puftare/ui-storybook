dc53c793644d3b6686fae94b0962c65eb78c8efe
puftare <martin.potkonjak@hotmail.com>
2024-06-07 13:23:38 +0200

    change: storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,14 +10,15 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
+    "@storybook/addon-postcss",
+    // {
+    //   name: "@storybook/addon-postcss",
+    //   options: {
+    //     postcssLoaderOptions: {
+    //       implementation: require("postcss"),
+    //     },
+    //   },
+    // },
   ],
   framework: {
     name: "@storybook/react-webpack5",

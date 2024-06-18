ff66423fd576cda96716cb82f984df7cc54d46b1
puftare <martin.potkonjak@hotmail.com>
2024-06-18 12:23:47 +0200

    fix-import: Fixed Sidebar named import

--- src/components/Sidebar/Sidebar.stories.tsx
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryFn } from "@storybook/react";
-import Sidebar, { SidebarProps } from "./Sidebar";
+import { Sidebar, SidebarProps } from "./Sidebar";
 import {
   AppWindow,
   SettingsIcon,
